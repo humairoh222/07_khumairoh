e129b43720e2cf365994013d031d2710dc6020d9
violinkusuma38-svg <violin.kusuma38@smk.belajar.id>
2025-08-20 21:44:15 +0700

    segitiga

---
+++ luas_segitiga.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>Luas Segitiga</h1>";
+
+$alas = 12;
+$tinggi = 8;
+$luas = 0.5 * $alas * $tinggi;
+
+echo "<b>Rumus:</b> L = 1/2 × a × t <br>";
+echo "<b>Soal:</b> Hitung luas segitiga dengan alas $alas cm dan tinggi $tinggi cm.<br>";
+echo "<b>Jawaban:</b> 1/2 × $alas × $tinggi = $luas cm²";
+?>
