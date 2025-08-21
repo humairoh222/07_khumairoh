9dda61ab48d46fdaa9e65c6dcfc0b3bff9a976f4
violinkusuma38-svg <violin.kusuma38@smk.belajar.id>
2025-08-21 11:52:23 +0700

    kecepatan

---
+++ kecepatan.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>Rumus Kecepatan</h1>";
+
+$jarak = 120; // km
+$waktu = 2;  // jam
+$kecepatan = $jarak / $waktu;
+
+echo "<b>Rumus:</b> v = s / t <br>";
+echo "<b>Soal:</b> Sebuah mobil menempuh jarak $jarak km dalam waktu $waktu jam. Hitung kecepatannya.<br>";
+echo "<b>Jawaban:</b> $jarak ÷ $waktu = $kecepatan km/jam";
+?>
