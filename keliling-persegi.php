340121da5a4040494ca72f77a074691a4396e756
violinkusuma38-svg <violin.kusuma38@smk.belajar.id>
2025-08-21 11:42:15 +0700

    keliling

---
+++ keliling-persegi.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1>Keliling Persegi</h1>";
+
+$sisi = 9;
+$keliling = 4 * $sisi;
+
+echo "<b>Rumus:</b> K = 4 × s <br>";
+echo "<b>Soal:</b> Hitung keliling persegi dengan sisi $sisi cm.<br>";
+echo "<b>Jawaban:</b> 4 × $sisi = $keliling cm";
+?>
